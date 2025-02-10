20b5b84214441702e8f41f85661af852927cb8d9
Pablo <dexpb@hotmail.com>
2025-02-10 10:25:52 -0300

    feat: add use client in services.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
-
-
 import { ServicesDiv } from "@/components/sevices";
 import { BestOptions } from "./../components/best-options";
 import SwiperImages from "@/components/swiper";
-
-
 import { Comments } from "@/components/comments";
 import { ContactUs } from "@/components/contact-us";
 import { Footer } from "@/components/footer";
